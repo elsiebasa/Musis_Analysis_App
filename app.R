4de9a8a43e2b4f9098d0940efe01a34b722cab35
elsiebasa <efbasa@ucdavis.edu>
2020-03-08 16:55:28 -0700

    Commit

--- app.R
+++ app.R
@@ -6,12 +6,14 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+library(tidytext)
+library(textdata)
 library(lubridate)
 library(httr)
 library(tidyverse)
 library(jsonlite)
 library(shiny)
+library(ggplot2)
 get_info <- function(artist, title){
   fromJSON((
     str_glue("https://api.vagalume.com.br/search.php?apikey=114e5edf0076c875c605607fa7b82eec&art={artist}&mus={title}&extra=relart,artpic" , artist = str_replace_all(artist , c(" " = "%20")),
@@ -19,46 +21,99 @@ get_info <- function(artist, title){
     ))
   )
 }
+# sentiment analysis
+
 
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Music Lyrics App"),
-   
-
-   sidebarLayout(
-      sidebarPanel(
-        textInput("artist","artist", width = NULL,
-                  placeholder = "e.g Justin Bieber"
-        ),
-        textInput("song","song", width = NULL,
-                  placeholder = "e.g Baby"
-        )
+  #suppress warings while wating for inputs
+  tags$style(type="text/css",
+             ".shiny-output-error { visibility: hidden; }",
+             ".shiny-output-error:before { visibility: hidden; }"
+  ),
+  # Application title
+  titlePanel("Music Lyrics App"),
+  
+  
+  sidebarLayout(
+    sidebarPanel(
+      textInput("artist","artist", width = NULL,
+                placeholder = "e.g Justin Bieber"
       ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-
-         fluidRow(column(10, verbatimTextOutput("lyric")))
-         
+      textInput("song","song", width = NULL,
+                placeholder = "e.g Baby"
+      ),
+      checkboxGroupInput("checkboxes", "Choose:",
+                         choiceNames =
+                           list("Sentiment Analysis Chart"),
+                         choiceValues =
+                           list("sentiment")
       )
-   )
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      
+      fluidRow(column(10, verbatimTextOutput("lyric"))),
+      plotOutput("pie")
+      
+    )
+  )
+  
 )
 
 
 server <- function(input, output) {
-   
-
-
-
-   
-   output$lyric <- renderText({ 
-     lyrics <- get_info(input$artist, input$song)$mus$text
-     
-   })
-   
+  
+  options(warn = -1) 
+  
+  output$lyric <- renderText({ 
+    lyric <- get_info(input$artist, input$song)$mus$text
+    
+  })
+  
+  sentiment_analysis <-  reactive({
+    lines<- get_info(input$artist, input$song)$mus$text %>%
+      str_split("\n") %>% 
+      unlist() 
+    lyric_df<- tibble(line = 1:length(lines), lines)
+    
+    token <-  lyric_df %>% unnest_tokens(word, lines) %>%
+      anti_join(stop_words)
+    
+    
+    sentiment <- token %>% left_join(get_sentiments("nrc")) %>% 
+      filter(sentiment %in% c("positive", "negative" )) %>% 
+      group_by(line) %>% 
+      summarize(
+        positive = sum(sentiment == "positive", na.rm = TRUE), 
+        negative = sum(sentiment == "negative", na.rm = TRUE), 
+        netural = n() - positive - negative) %>%
+      mutate(
+        line,
+        sentiment = case_when(
+          positive > negative ~ "positive",
+          positive < negative ~ "negative",
+          TRUE ~ "netural"
+        )
+      )
+    sentiment %>% count(sentiment)
+    
+  })
+  output$pie <- renderPlot({ 
+    if ("sentiment" %in% input$checkboxes) {
+      df_pie = sentiment_analysis()
+      bp<- ggplot( df_pie, aes(x="", y=n, fill=sentiment)) +geom_bar(width = 1, stat = "identity")
+      pie <- bp + coord_polar("y", start=0)
+      pie
+    }
+    
+  })  
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
 
